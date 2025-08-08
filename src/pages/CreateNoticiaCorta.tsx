5ffa2c93fe2643978998a763d1899a3c53bb90d3
Rodrigo Arce Diaz <rarce2002@gmail.com>
2025-08-08 11:55:27 -0700

    Maybe fixed

--- src/pages/CreateNoticiaCorta.tsx
+++ src/pages/CreateNoticiaCorta.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { generateClient } from "aws-amplify/data";
-import type { Schema } from "../amplify/data/resource";
+import type { Schema } from "../../amplify/data/resource";
 import { useNavigate } from "react-router-dom";
 
 const client = generateClient<Schema>();
