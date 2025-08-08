b0dd5e4b875b73b79ce31cd57a885cca38525517
Rodrigo Arce Diaz <rarce2002@gmail.com>
2025-08-08 11:09:35 -0700

    Check if backend works, simple front end

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
-
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
+export default function App() {
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+    <main style={{ padding: 20, textAlign: "center" }}>
+      <h1>Amplify Backend Deployment Test</h1>
+      <p>If you see this, the app deployed successfully.</p>
     </main>
   );
 }
-
-export default App;
